fad6e53bc80ffead120a38b8a61708a3c01cb7c0
KuanLin <isu10122068a@gmail.com>
2023-06-10 21:56:46 +0800

    Initial Commit

---
+++ myblog/myblog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-+u1ez4ugu2x_^%kn*^ppm5cspm_7v07dp9sc_)(73ox_4e376n"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
